279e3d4524901ecc5f959fdc2c791e9ae7fa8624
Aldrin Montana <octalene.dev@pm.me>
2024-04-29 15:31:04 -0700

    minor: style changes and updated Arrow
    Commit ID for Arrow was updated to Arrow 17.0.0 from Arrow 9.0.0 (wow,
    old)

--- src/arrow_extension.cpp
+++ src/arrow_extension.cpp
@@ -6,39 +6,41 @@
 #include "arrow_to_ipc.hpp"
 
 #include "duckdb.hpp"
+
 #ifndef DUCKDB_AMALGAMATION
-#include "duckdb/common/arrow/result_arrow_wrapper.hpp"
-#include "duckdb/common/arrow/arrow_appender.hpp"
-#include "duckdb/common/arrow/arrow_converter.hpp"
-#include "duckdb/main/extension_util.hpp"
-#include "duckdb/parser/parsed_data/create_table_function_info.hpp"
-#include "duckdb/function/table/arrow.hpp"
+  #include "duckdb/common/arrow/result_arrow_wrapper.hpp"
+  #include "duckdb/common/arrow/arrow_appender.hpp"
+  #include "duckdb/common/arrow/arrow_converter.hpp"
+  #include "duckdb/main/extension_util.hpp"
+  #include "duckdb/parser/parsed_data/create_table_function_info.hpp"
+  #include "duckdb/function/table/arrow.hpp"
 #endif
 
+
+// ------------------------------
+// Load extension automatically (I think)
 namespace duckdb {
 
-static void LoadInternal(DatabaseInstance &instance) {
-  ExtensionUtil::RegisterFunction(instance, ToArrowIPCFunction::GetFunction());
-  ExtensionUtil::RegisterFunction(instance,
-                                  ArrowIPCTableFunction::GetFunction());
-}
+  static void LoadInternal(DatabaseInstance &instance) {
+    ExtensionUtil::RegisterFunction(instance,    ToArrowIPCFunction::GetFunction());
+    ExtensionUtil::RegisterFunction(instance, ArrowIPCTableFunction::GetFunction());
+  }
 
-void ArrowExtension::Load(DuckDB &db) { LoadInternal(*db.instance); }
-std::string ArrowExtension::Name() { return "arrow"; }
+  void        ArrowExtension::Load(DuckDB &db) { LoadInternal(*db.instance); }
+  std::string ArrowExtension::Name()           { return "arrow"; }
 
-} // namespace duckdb
+} // namespace: duckdb
+
 
 extern "C" {
+  DUCKDB_EXTENSION_API
+  void arrow_init(duckdb::DatabaseInstance &db) { LoadInternal(db); }
 
-DUCKDB_EXTENSION_API void arrow_init(duckdb::DatabaseInstance &db) {
-  LoadInternal(db);
+  DUCKDB_EXTENSION_API
+  const char *arrow_version() { return duckdb::DuckDB::LibraryVersion(); }
 }
 
-DUCKDB_EXTENSION_API const char *arrow_version() {
-  return duckdb::DuckDB::LibraryVersion();
-}
-}
 
 #ifndef DUCKDB_EXTENSION_MAIN
-#error DUCKDB_EXTENSION_MAIN not defined
+  #error DUCKDB_EXTENSION_MAIN not defined
 #endif
